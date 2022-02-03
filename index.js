1dcc7c030859b00de8bb68fa4492c23c00dcb2cb
Marcos Heleno <marcosheleno92@gmail.com>
2022-02-03 14:17:35 -0300

    Heroku file

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/about.html'));
+});
+
+router.get('/sitemap',function(req,res){
+  res.sendFile(path.join(__dirname+'/sitemap.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.PORT || 3000);
