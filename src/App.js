48765b037145fdae3c9b7a971950d71c136af9b5
Marcos Heleno <marcosheleno92@gmail.com>
2022-02-03 21:37:18 -0300

    Dojo tests

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import Hello from "./components/Hello";
+import LandPage from "./components/LandPage";
+
+function App() {
+  return (
+    <LandPage/>
+  );
+}
+
+export default App;
---
+++ src/components/Contacts.js
@@ -0,0 +1,7 @@
+import React from "react";
+export default function Contacts(props) {
+  return (<>
+    <h3>Sua Lista vem aqui</h3>
+  </>
+  );
+}
