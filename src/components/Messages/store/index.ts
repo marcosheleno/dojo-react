eca5e6206dc75e9978d153d9e9bfc40f3c1d2e57
joaovmr <48189739+joaovmr@users.noreply.github.com>
2022-02-10 15:29:29 -0300

    fixing message

--- src/components/Messages/store/index.ts
+++ src/components/Messages/store/index.ts
@@ -27,36 +27,7 @@ export interface TPageState {
 const reducers = (state: IInitalState = INITIAL_STATE, action: TAction) => {
     switch (action.type) {
         case ADD_MESSAGE:
-            console.log(state);
-
-
-            if (!state.hasOwnProperty('chats')) {
-                state.chats = [];
-            }
-
-            //criar indice no state
-            if (!state.chats[action.payload.id]) {
-                state.chats[action.payload.id] = action.payload;
-
-                return state;
-            }
-            //concatenar
-
-
-
-            state.chats.map((chat, index) => {
-                if (index !== action.payload.id) {
-                    return chat;
-                }
-
-                chat.messages.concat(action.payload.messages);
-
-                return chat;
-            })
-
-            // state.chats[action.payload.id].messages.concat(action.payload.messages);
-
-            return state;
+            return { ...state, chats: state.chats?.concat(action.payload) };
         default:
             return state;
     }
