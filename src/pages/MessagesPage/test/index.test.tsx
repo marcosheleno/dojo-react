bd53aa76da6e22420a57276d30900b2e2dc5ea47
joaovmr <48189739+joaovmr@users.noreply.github.com>
2022-02-11 09:59:44 -0300

    Fixing Tests

--- src/pages/MessagesPage/test/index.test.tsx
+++ src/pages/MessagesPage/test/index.test.tsx
@@ -1,46 +1,58 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { TMessage } from '../../../components/Messages/contract';
-import MessagesList from '../../../components/Messages';
-import MessageBox from '../../../components/Messages/MessageBox';
-import { Provider } from 'react-redux';
-import store from '../../../store/store';
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { TMessage } from "../../../components/Messages/contract";
+import MessagesList from "../../../components/Messages";
+import MessageBox from "../../../components/Messages/MessageBox";
+import { Provider } from "react-redux";
+import store from "../../../store/store";
 
-describe('when no messages are available', () => {
-    it('show a default message', () => {
-        render(<MessagesList messages={[]} />);
-        const noMessages = screen.getByText(
-            'Oi, gostaria de falar uma coisa contigo!'
-        );
-        expect(noMessages).toBeInTheDocument();
-    });
+describe("when no messages are available", () => {
+  it("show a default message", () => {
+    
+    const emptyMessage:TMessage[] = [
+        {
+            body: "Oi, gostaria de falar uma coisa contigo!",
+            date: new Date()
+        }
+    ]
+    render(
+      <Provider store={store}>
+        <MessagesList messages={emptyMessage} />
+      </Provider>
+    );
+
+    const noMessages = screen.getByText(
+      "Oi, gostaria de falar uma coisa contigo!"
+    );
+    expect(noMessages).toBeInTheDocument();
+  });
 });
 
-describe('when messages are available, show them', () => {
-    it('has a single message', () => {
-        const mockMessage: TMessage = { body: 'Boa tarde!', date: new Date() };
-        const mockMessages = [mockMessage];
+describe("when messages are available, show them", () => {
+  it("has a single message", () => {
+    const mockMessage: TMessage = { body: "Boa tarde!", date: new Date() };
+    const mockMessages = [mockMessage];
 
-        render(<MessagesList messages={mockMessages} />);
-        const message = screen.getByText('Boa tarde!');
+    render(<MessagesList messages={mockMessages} />);
+    const message = screen.getByText("Boa tarde!");
 
-        expect(message).toBeInTheDocument();
-    });
+    expect(message).toBeInTheDocument();
+  });
 });
 
-describe('send messages', () => {
-    it('send a test message', () => {
-        const messageText = 'Testando o campo';
+describe("send messages", () => {
+  it("send a test message", () => {
+    const messageText = "Testando o campo";
 
-        render(
-            <Provider store={store}>
-                <MessageBox />
-            </Provider>
-        );
+    render(
+      <Provider store={store}>
+        <MessageBox />
+      </Provider>
+    );
 
-        const inputField = screen.getByTestId('message-box');
-        fireEvent.change(inputField, { target: { value: messageText } });
-        const sendButton = screen.getByTestId('message-send');
-        fireEvent.click(sendButton);
-    });
+    const inputField = screen.getByTestId("message-box");
+    fireEvent.change(inputField, { target: { value: messageText } });
+    const sendButton = screen.getByTestId("message-send");
+    fireEvent.click(sendButton);
+  });
 });
