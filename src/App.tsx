4e4b5c01e6c1fcd8f88b2e94f39c988028a4bb01
joaovmr <48189739+joaovmr@users.noreply.github.com>
2022-02-11 16:32:52 -0300

    translating the app

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,26 @@ import './styles/global.css';
 function App() {
     const id = useSelector((state: TReducers) => state.page.id);
 
+    
+    const locales:any = {
+        en: 'English',
+        fr: 'French',
+        ar: 'Arabic',
+        de: 'German',
+        zh: 'Chinese'
+    }
+    locale: navigator.language
+
     if (id) {
         return <MessagesPage contactId={id} />;
     }
     return (
         <>
+            <select>
+          {Object.keys(locales).map((key) => {
+            return <option value={key} key={key}>{locales[key]}</option>
+          })}
+        </select>
             <ContactsPage />;
             <LandPage />
         </>
---
+++ src/components/i18n/i18n.translations.ts
@@ -0,0 +1,27 @@
+import { namespaces } from "./i18n.constants";
+
+export const pt = {
+  [namespaces.common]: {
+    buttons: {
+        send: "Enviar",
+        return: "Voltar",
+    },
+  },
+  [namespaces.pages.contacts]: {
+    groups: "Grupos",
+    emails: "Emails",
+    phones: "Telefones",
+  },
+};
+
+export const en = {
+  [namespaces.common]: {
+    buttons: {
+        send: "Ok",
+      cancel: "Cancel",
+    },
+  },
+  [namespaces.pages.hello]: {
+    welcome: "Welcome",
+  },
+};
